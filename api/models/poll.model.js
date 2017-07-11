5562e8d3136abafe5c8ccd5fdd25214d7a9885cc
Mateusz Szmytko <mateusz.szmytko@gmail.com>
2017-07-11 10:48:17 +0200

    cleaning

--- api/models/poll.model.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var mongoose_1 = require("mongoose");
-var poll_schema_1 = require("../schemas/poll.schema");
-exports.Poll = mongoose_1.model("Poll", poll_schema_1.PollSchema);
-//# sourceMappingURL=poll.model.js.map
