922856cf776da0e0829f3d18e8df39bc8972d79f
Mateusz Szmytko <mateusz.szmytko@gmail.com>
2017-09-18 15:18:50 +0200

    security fix

--- api/app.ts
+++ api/app.ts
@@ -45,9 +45,9 @@ class App {
     this.express.use(bodyParser.json());
     this.express.use(bodyParser.urlencoded({ extended: false }));
     this.express.use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Origin", "https://raa-polls.herokuapp.com/");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        res.header('Access-Control-Allow-Methods', "GET, POST, OPTIONS, PUT, PATCH, DELETE");
         next();
     });
     this.express.use(express.static(path.join(__dirname, '../dist')));
